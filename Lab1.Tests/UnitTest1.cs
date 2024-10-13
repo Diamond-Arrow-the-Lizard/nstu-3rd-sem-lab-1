89342e3a6d5f67ca42fe05d57dda6f5d962c6e87
Diamond-Arrow-the-Lizard <diamond1arrow@gmail.com>
2024-10-13 21:45:01 +0700

    Made Unit Test for the UI input of choice cases

--- Lab1.Tests/UnitTest1.cs
+++ Lab1.Tests/UnitTest1.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab1.Tests {
 
@@ -7,7 +11,7 @@ namespace Lab1.Tests {
         [Theory]
         [InlineData(null, null, 0)] 
             [InlineData("ABC", "GREEN", 10000000)] 
-                public virtual void PrintTest(string Model, String Color, int Price) {
+                public virtual void Print_Should_NotCrashProgram(string Model, String Color, int Price) {
 
                     Console.WriteLine(String.Format(" Model: {0}\n Color: {1}\n Price: {2}",
                                 Model, Color, Price));
@@ -15,141 +19,61 @@ namespace Lab1.Tests {
 
     }
 
-    public class ProgramTests {
-        
-        [Theory]
-        [InlineData(0, 0)]
-        [InlineData(4, 8)]
-        [InlineData(6, 10)]
-        public static void UI(int Choice, int Iterations) {
-            List<MobileDevices> DevicesList = new List<MobileDevices>();
+    public class ProgramUITests {
 
-            while(Iterations != 0) {
-                Console.WriteLine("Выберите действие со списком:\n 0) Закончить изменения\n 1) Добавить мобильное устройство\n 2) Добавить смартфон\n 3) Добавить электронную книгу\n 4) Напечатать список");
-                Console.Write("==> ");
+        [Fact]
+        public static void UI_Should_NotCrashProgram() {
+            for (int Choice = -100000; Choice < 100000; ++Choice) {
 
                 switch(Choice) {
 
                     case 0:
-                        System.Environment.Exit(0);
                         break;
                     case 1:
                         Console.Write("Color: ");
 
-                        string? newColor = " ";
-                        if(newColor != null)
-                            newColor = Console.ReadLine();
-
                         Console.Write("Price: ");
-
-                        float newPrice = 0.0f;
-                        newPrice = (float)Convert.ToDouble(Console.ReadLine());
 
                         Console.Write("Model : ");
 
-                        string? newModel = " "; 
-                        if(newModel != null)
-                            newModel = Console.ReadLine();
 
-                        MobileDevices newDev = new MobileDevices() {
-
-                            Color = newColor,
-                                  Price = newPrice,
-                                  Model = newModel
-                        };
-
-                        DevicesList.Add(newDev);
                         break;
 
                     case 2:
                         Console.Write("Color: ");
 
-                        newColor = " ";
-                        if(newColor != null)
-                            newColor = Console.ReadLine();
-
                         Console.Write("Model : ");
-
-                        newModel = " "; 
-                        if(newModel != null)
-                            newModel = Console.ReadLine();
 
                         Console.Write("Price: ");
 
-                        newPrice = 0.0f;
-                        newPrice = (float)Convert.ToDouble(Console.ReadLine());
-
                         Console.Write("Firm: ");
-                        string? newFirm = " ";
-                        if (newFirm != null)
-                            newFirm = Console.ReadLine();
 
-                        Smartphone newPhone = new Smartphone() {
-
-                            Color = newColor,
-                                  Price = newPrice,
-                                  Model = newModel,
-                                  Firm = newFirm
-
-                        };
-
-                        DevicesList.Add(newPhone);
                         break;
 
                     case 3:
                         Console.Write("Color: ");
 
-                        newColor = " ";
-                        if(newColor != null)
-                            newColor = Console.ReadLine();
-
                         Console.Write("Model : ");
-
-                        newModel = " "; 
-                        if(newModel != null)
-                            newModel = Console.ReadLine();
 
                         Console.Write("Price: ");
 
-                        newPrice = 0.0f;
-                        newPrice = (float)Convert.ToDouble(Console.ReadLine());
-
                         Console.Write("Memory size: ");
 
-                        int newValue = (int)Convert.ToInt32(Console.ReadLine());
-
                         Console.Write("Has backlight: ");
-                        bool hasBlight = Convert.ToBoolean(Console.ReadLine());
 
-                        ElectronicBook newEbook = new ElectronicBook() {
-
-                            Color = newColor,
-                                  Price = newPrice,
-                                  Model = newModel,
-                                  Value = newValue,
-                                  hasBacklight = hasBlight
-
-                        };
-
-                        DevicesList.Add(newEbook);
                         break;
 
                     case 4:
-                        foreach(MobileDevices Device in DevicesList) {
-                            Device.Print();
-                        }
+                        Console.WriteLine("List Written");
                         break;
-
 
 
                     default:
                         Console.WriteLine("Wrong Input. Please try again.\n\n\n");
                         break;
 
-
                 }
-                
-                --Iterations;
+
             }
 
         }
@@ -157,3 +81,5 @@ namespace Lab1.Tests {
     }
 
 }
+
+
--- Program/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148fe2be130ebecc992b898b3043834f7dffbbbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
